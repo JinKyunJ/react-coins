44deb6e78f114e1a94e4a940314b7a43185b412e
JinkyunJ <148345588+JinKyunJ@users.noreply.github.com>
2024-07-03 11:03:19 +0900

    nomad-coders react Coin Tracker

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [loading, setLoading] = useState(true); // 데이터 로딩
+    const [coins, setCoins] = useState([]); // 모든 코인 데이터 저장
+    const [userInput, setUserInput] = useState(''); // 사용자 입력값 저장
+    const [searchResults, setSearchResults] = useState([]); // 검색 결과를 저장
+
+    useEffect(() => {
+        fetch('https://api.coinpaprika.com/v1/tickers')
+            .then((response) => response.json())
+            .then((json) => {
+                setCoins(json);
+                setLoading(false);
+                // console.log(json.map((coin) => coin.symbol));
+            });
+    }, []);
+
+    const handleSearch = (event) => {
+        const inputValue = event.target.value.toLowerCase(); // 입력값을 소문자로 변환
+        setUserInput(inputValue);
+        if (inputValue.trim() === '') {
+            setSearchResults([]); // 입력값이 없으면 검색 결과 빈 배열
+        } else {
+            const results = coins.filter((coin) => coin.symbol.toLowerCase().includes(inputValue));
+            setSearchResults(results);
+        }
+    };
+
+    return (
+        <div className="coinContainer">
+            <h1>The Coins {loading ? '' : `: ${coins.length}`}</h1>
+            {loading ? (
+                <strong>Loading...</strong>
+            ) : (
+                <select>
+                    {coins.map((coin, index) => (
+                        <option key={coin.id}>
+                            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+                        </option>
+                    ))}
+                </select>
+            )}
+            <hr />
+            <input
+                className="coinInput"
+                onChange={handleSearch}
+                value={userInput}
+                type="text"
+                placeholder="Search coins"
+            />
+            <div className="coinResult">
+                {searchResults.map((coin) => (
+                    <div key={coin.id}>
+                        <p>
+                            {coin.name} ({coin.symbol}): ${coin.quotes.USD.price} USD
+                        </p>
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default App;
